b67cb4588089873776cb6e760341dd08f9111aa0
Rakibul Islam <rakib.ca@gmail.com>
2024-02-25 01:43:53 -0700

    remove first element: added

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,19 @@ const INITIAL_VALUE = ["A", "B", "C"];
 
 function App() {
   const [array, setArray] = useState(INITIAL_VALUE);
+
+  function removeFirstElement() {
+    setArray((currentArray) => {
+      return currentArray.slice(1);
+    });
+  }
+
   return (
     <div>
-      <h1>Helloooo</h1>
+      <button onClick={removeFirstElement}>Remove First Element</button>
+      <br />
+      <br />
+      <div>{array.join(",")}</div>
     </div>
   );
 }
